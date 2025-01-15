65a17436378e02309235f41305674a46d232e1c5
Andrey de Abreu <54045198+dreymond1@users.noreply.github.com>
2025-01-15 10:33:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, request, jsonify
+import tensorflow as tf
+import pickle
 
 # Inicializa o aplicativo Flask
 app = Flask(__name__)
+
+# Função para carregar recursos do modelo
+def carregar_recursos():
+    # Carregando o modelo TFLite
+    interpreter = tf.lite.Interpreter(model_path="files/sentiment_model.tflite")
+    interpreter.allocate_tensors()
+
+    # Carregando tokenizer e label encoder
+    with open("files/tokenizer.pkl", "rb") as f:
+        tokenizer = pickle.load(f)
+    with open("files/label_encoder.pkl", "rb") as f:
+        label_encoder = pickle.load(f)
+
+    return interpreter, tokenizer, label_encoder
+
+# Carrega os recursos uma vez no início
+interpreter, tokenizer, label_encoder = carregar_recursos()
 
 # Rota para a API
 @app.route("/")
